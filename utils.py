d5cb262c40e538d4b336926f32c5817eafa8dd85
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-12-10 20:10:41 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -379,7 +379,7 @@ def humanbytes(size):
 
 #shortzy
 
-shortz = shortzy.Shortzy(SHORTENER_API, "droplink.co")
+shortz = shortzy.Shortzy(SHORTENER_API, "https://shareus.io")
 async def get_shortlink(link):
          if  SHORTENER_API:
                if  LONG_DROPLINK_URL =="True" or LONG_DROPLINK_URL is True:
