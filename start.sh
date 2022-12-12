2fe830b7db6b1d76c07afa11dcee1889a283c45e
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-12-12 09:26:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Dkmovie/test.git /test
-cd /test
+git clone -b master https://github.com/Dkmovie/IMDB-Autofilter-Link-Shortener-bot.git /IMDB-Autofilter-Link-Shortener-bot
+cd /IMDB-Autofilter-Link-Shortener-bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
